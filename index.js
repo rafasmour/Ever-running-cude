e978887ea8ec8c3500e47399a29906ba86c7cd38
rafasmour <rafasmour@gmail.com>
2024-11-08 09:13:21 +0200

    hi

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use( '^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, './public/html/index.html'));
 })
 
 app.all('*', (req, res) => {
